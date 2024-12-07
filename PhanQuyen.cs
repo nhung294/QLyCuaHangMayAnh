62e0c7407babcd1381129eb31047ccbb2e268dad
TranThiVy <tranthivy17112004@gmail.com>
2024-12-07 23:04:03 +0700

    Add files via upload

---
+++ PhanQuyen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTapLon.HamPhuTro
+{
+    public class PhanQuyen
+    {
+        public static PhanQuyen instance;
+        public string MaNV { get; set; }
+        public string CongViec {  get; set; }
+
+
+        private PhanQuyen() { }
+
+        public static PhanQuyen Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = new PhanQuyen();
+                }
+                return instance;
+            }
+        }
+    }
+}
