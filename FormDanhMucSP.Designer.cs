77f918fd39cdb8744a7f0a5764395240cadd1867
TranThiVy <tranthivy17112004@gmail.com>
2024-12-07 15:00:49 +0700

    Quản lý danh mục sản phẩm

---
+++ FormDanhMucSP.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace BaiTapLon.Forms
+{
+    partial class FormDanhMucSP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBoxTenLoai = new System.Windows.Forms.TextBox();
+            this.labelMaLoai = new System.Windows.Forms.Label();
+            this.labelTenLoai = new System.Windows.Forms.Label();
+            this.textBoxMaLoai = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.dataGridViewLoaiHang = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonSuaLoai = new System.Windows.Forms.Button();
+            this.buttonThemLoai = new System.Windows.Forms.Button();
+            this.buttonThoat = new System.Windows.Forms.Button();
+            this.buttonLamMoi = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLoaiHang)).BeginInit();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 27.27273F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 45.45455F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 27.27273F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1443, 50);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(585, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(270, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DANH MỤC SẢN PHẨM";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 50);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1443, 34);
+            this.panel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(631, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Thông tin chung";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 19.50248F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30.51768F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 19.50248F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30.47735F));
+            this.tableLayoutPanel2.Controls.Add(this.textBoxTenLoai, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.labelMaLoai, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.labelTenLoai, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.textBoxMaLoai, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 84);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1443, 42);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // textBoxTenLoai
+            // 
+            this.textBoxTenLoai.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBoxTenLoai.Location = new System.Drawing.Point(1045, 10);
+            this.textBoxTenLoai.Name = "textBoxTenLoai";
+            this.textBoxTenLoai.Size = new System.Drawing.Size(355, 22);
+            this.textBoxTenLoai.TabIndex = 3;
+            // 
+            // labelMaLoai
+            // 
+            this.labelMaLoai.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelMaLoai.AutoSize = true;
+            this.labelMaLoai.Location = new System.Drawing.Point(113, 13);
+            this.labelMaLoai.Name = "labelMaLoai";
+            this.labelMaLoai.Size = new System.Drawing.Size(54, 16);
+            this.labelMaLoai.TabIndex = 0;
+            this.labelMaLoai.Text = "Mã loại:";
+            // 
+            // labelTenLoai
+            // 
+            this.labelTenLoai.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelTenLoai.AutoSize = true;
+            this.labelTenLoai.Location = new System.Drawing.Point(832, 13);
+            this.labelTenLoai.Name = "labelTenLoai";
+            this.labelTenLoai.Size = new System.Drawing.Size(59, 16);
+            this.labelTenLoai.TabIndex = 1;
+            this.labelTenLoai.Text = "Tên loại:";
+            // 
+            // textBoxMaLoai
+            // 
+            this.textBoxMaLoai.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBoxMaLoai.Enabled = false;
+            this.textBoxMaLoai.Location = new System.Drawing.Point(323, 10);
+            this.textBoxMaLoai.Name = "textBoxMaLoai";
+            this.textBoxMaLoai.Size = new System.Drawing.Size(355, 22);
+            this.textBoxMaLoai.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 126);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1443, 39);
+            this.panel2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(578, 2);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(273, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Danh sách danh mục sản phẩm";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.dataGridViewLoaiHang);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 165);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1443, 356);
+            this.panel3.TabIndex = 4;
+            // 
+            // dataGridViewLoaiHang
+            // 
+            this.dataGridViewLoaiHang.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewLoaiHang.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewLoaiHang.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewLoaiHang.Name = "dataGridViewLoaiHang";
+            this.dataGridViewLoaiHang.RowHeadersWidth = 51;
+            this.dataGridViewLoaiHang.RowTemplate.Height = 24;
+            this.dataGridViewLoaiHang.Size = new System.Drawing.Size(1443, 356);
+            this.dataGridViewLoaiHang.TabIndex = 0;
+            this.dataGridViewLoaiHang.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewLoaiHang_CellClick);
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 4;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel3.Controls.Add(this.buttonLamMoi, 2, 0);
+            this.tableLayoutPanel3.Controls.Add(this.buttonSuaLoai, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.buttonThemLoai, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.buttonThoat, 3, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 521);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 59F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(1443, 59);
+            this.tableLayoutPanel3.TabIndex = 5;
+            // 
+            // buttonSuaLoai
+            // 
+            this.buttonSuaLoai.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonSuaLoai.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSuaLoai.Location = new System.Drawing.Point(463, 14);
+            this.buttonSuaLoai.Name = "buttonSuaLoai";
+            this.buttonSuaLoai.Size = new System.Drawing.Size(153, 30);
+            this.buttonSuaLoai.TabIndex = 1;
+            this.buttonSuaLoai.Text = "Sửa DMSP";
+            this.buttonSuaLoai.UseVisualStyleBackColor = true;
+            this.buttonSuaLoai.Click += new System.EventHandler(this.buttonSuaLoai_Click);
+            // 
+            // buttonThemLoai
+            // 
+            this.buttonThemLoai.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonThemLoai.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonThemLoai.Location = new System.Drawing.Point(103, 14);
+            this.buttonThemLoai.Name = "buttonThemLoai";
+            this.buttonThemLoai.Size = new System.Drawing.Size(153, 30);
+            this.buttonThemLoai.TabIndex = 0;
+            this.buttonThemLoai.Text = "Thêm DMSP";
+            this.buttonThemLoai.UseVisualStyleBackColor = true;
+            this.buttonThemLoai.Click += new System.EventHandler(this.buttonThemLoai_Click);
+            // 
+            // buttonThoat
+            // 
+            this.buttonThoat.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonThoat.Location = new System.Drawing.Point(1185, 14);
+            this.buttonThoat.Name = "buttonThoat";
+            this.buttonThoat.Size = new System.Drawing.Size(153, 30);
+            this.buttonThoat.TabIndex = 3;
+            this.buttonThoat.Text = "Thoát";
+            this.buttonThoat.UseVisualStyleBackColor = true;
+            this.buttonThoat.Click += new System.EventHandler(this.buttonThoat_Click);
+            // 
+            // buttonLamMoi
+            // 
+            this.buttonLamMoi.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonLamMoi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLamMoi.Location = new System.Drawing.Point(823, 14);
+            this.buttonLamMoi.Name = "buttonLamMoi";
+            this.buttonLamMoi.Size = new System.Drawing.Size(153, 30);
+            this.buttonLamMoi.TabIndex = 4;
+            this.buttonLamMoi.Text = "Làm mới ";
+            this.buttonLamMoi.UseVisualStyleBackColor = true;
+            this.buttonLamMoi.Click += new System.EventHandler(this.buttonLamMoi_Click);
+            // 
+            // FormDanhMucSP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1443, 686);
+            this.Controls.Add(this.tableLayoutPanel3);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "FormDanhMucSP";
+            this.Text = "FormDanhMucSP";
+            this.Load += new System.EventHandler(this.FormDanhMucSP_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLoaiHang)).EndInit();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TextBox textBoxTenLoai;
+        private System.Windows.Forms.Label labelMaLoai;
+        private System.Windows.Forms.Label labelTenLoai;
+        private System.Windows.Forms.TextBox textBoxMaLoai;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.DataGridView dataGridViewLoaiHang;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button buttonThoat;
+        private System.Windows.Forms.Button buttonSuaLoai;
+        private System.Windows.Forms.Button buttonThemLoai;
+        private System.Windows.Forms.Button buttonLamMoi;
+    }
+}
---
+++ FormDanhMucSP.cs
@@ -0,0 +1,172 @@
+﻿using BaiTapLon.HamPhuTro;
+using BaiTapLon.KetNoiCSDL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTapLon.Forms
+{
+    public partial class FormDanhMucSP : Form
+    {
+        ThaoTacVoiCSDL dataProcessor = new ThaoTacVoiCSDL();
+        public FormDanhMucSP()
+        {
+            InitializeComponent();
+        }
+        //Sinh mã loại  
+        private String sinhMaLoai()
+        {   //LH01
+            String maLoaiNew = "LH";
+            DataTable dataTable = new DataTable();
+            dataTable = dataProcessor.DataReader("Select MaLoai from LOAIHANG ORDER BY MaLoai ASC");
+            if (dataTable.Rows.Count == 0) { return "LH01"; }
+            int[] maLoai = new int[dataTable.Rows.Count + 1];
+            //
+            foreach (DataRow row in dataTable.Rows)
+            {
+                int i = 0;
+                foreach (var item in row.ItemArray)
+                {
+                    String[] tam = item.ToString().Split('H');
+                    maLoai[i] = Convert.ToInt32(tam[1]);
+                    i++;
+                }
+            }
+            int max = maLoai.Max();
+            return maLoaiNew + (max + 1 < 10 ? "0" + (max + 1).ToString() : (max + 1).ToString());
+        }
+
+        private void LamMoi()
+        {
+            // Làm mới các trường nhập liệu
+            textBoxMaLoai.Text = sinhMaLoai();
+            textBoxTenLoai.Text = string.Empty;
+            buttonSuaLoai.Enabled = false;
+            buttonThemLoai.Enabled = true;
+            // Đặt con trỏ vào ô nhập tên loại
+            textBoxTenLoai.Focus();
+
+            // Lấy dữ liệu từ bảng LOAIHANG
+            string sqlGetLoaiHang = "SELECT * FROM LOAIHANG";
+            DataTable dataTableLoaiHang = dataProcessor.DataReader(sqlGetLoaiHang);
+
+            // Đặt dữ liệu vào DataGridView
+            dataGridViewLoaiHang.DataSource = dataTableLoaiHang;
+
+            // Tùy chỉnh lại cột trong DataGridView
+            dataGridViewLoaiHang.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewLoaiHang.Columns["MaLoai"].HeaderText = "Mã Loại";
+            dataGridViewLoaiHang.Columns["TenLoai"].HeaderText = "Tên Loại";
+        }
+
+        // Phương thức để tải dữ liệu từ cơ sở dữ liệu vào DataGridView
+
+        private void buttonThemLoai_Click(object sender, EventArgs e)
+        {
+            // Kiểm tra xem tên loại đã nhập chưa
+            if (textBoxTenLoai.Text.Trim() == string.Empty)
+            {
+                MessageBox.Show("Bạn chưa nhập tên loại!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBoxTenLoai.Focus();
+                return;
+            }
+
+            // Kiểm tra xem tên loại đã tồn tại chưa
+            string sqlKiemTraTenLoai = "SELECT * FROM LOAIHANG WHERE TenLoai = N'" + textBoxTenLoai.Text.Trim() + "'";
+            if (dataProcessor.DataReader(sqlKiemTraTenLoai).Rows.Count > 0)
+            {
+                MessageBox.Show("Tên loại này đã tồn tại!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBoxTenLoai.Focus();
+                return;
+            }
+
+            // Thêm loại hàng mới
+            string sqlThemLoaiHang = "INSERT INTO LOAIHANG (MaLoai, TenLoai) VALUES ('"
+                + textBoxMaLoai.Text.Trim() + "', N'" + textBoxTenLoai.Text.Trim() + "')";
+
+            dataProcessor.DataChange(sqlThemLoaiHang);
+
+            // Thông báo thêm thành công
+            MessageBox.Show("Bạn đã thêm loại hàng thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+            // Làm mới giao diện
+            this.LamMoi();
+        }
+
+        private void FormDanhMucSP_Load(object sender, EventArgs e)
+        {
+            this.LamMoi();
+        }
+
+
+
+        private void buttonThoat_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có muốn thoát không?", "Thông báo!", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                this.Close();
+                //FormTrangChu formTrangChu = new FormTrangChu();
+                //formTrangChu.Show();
+            }
+        }
+
+        private void buttonSuaLoai_Click(object sender, EventArgs e)
+        {
+            if (textBoxMaLoai.Text.Length == 0) 
+            {
+                MessageBox.Show("Bạn phải chọn loại hàng cần sửa");
+                return;
+            }
+            if(textBoxTenLoai.Text.Length == 0)
+            {
+                MessageBox.Show("Bạn phải nhập tên loại");
+                return;
+            }
+
+            string sqlLoaihang = "SELECT * FROM LoaiHang WHERE TenLoai = N'" + textBoxTenLoai.Text.Trim() + "'";
+            if (dataProcessor.DataReader(sqlLoaihang).Rows.Count > 1)
+            {
+                MessageBox.Show("loại hàng đã tồn tại!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string sqlUpdate = "UPDATE LoaiHang SET TenLoai = N'";
+            sqlUpdate = sqlUpdate + textBoxTenLoai.Text.Trim() + "' where MaLoai = '";
+            sqlUpdate = sqlUpdate + textBoxMaLoai.Text.Trim() + "'";
+
+            if (MessageBox.Show("Bạn có muốn sửa thông tin loại hàng này không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                dataProcessor.DataChange(sqlUpdate);
+                this.LamMoi();
+                MessageBox.Show("Bạn đã sửa thành công");
+            }
+        }
+
+        private void dataGridViewLoaiHang_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                DataGridViewRow row = dataGridViewLoaiHang.Rows[e.RowIndex];
+
+                // Gán giá trị từ các ô vào các TextBox
+                textBoxMaLoai.Text = Convert.ToString(row.Cells["MaLoai"].Value);
+                textBoxTenLoai.Text = Convert.ToString(row.Cells["TenLoai"].Value);
+                buttonThemLoai.Enabled = false;
+                buttonSuaLoai.Enabled = true;
+            }
+        }
+
+        private void buttonLamMoi_Click(object sender, EventArgs e)
+        {
+            this.LamMoi();
+        }
+    }
+}
